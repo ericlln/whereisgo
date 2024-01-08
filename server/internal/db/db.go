7727039b885bea221e1b29c5a4a1a9bf155891a8
Eric Lin <es3lin@uwaterloo.ca>
2024-01-07 19:07:34 -0500

    Move all db code to main project

--- server/internal/db/db.go
+++ server/internal/db/db.go
@@ -1,10 +1,37 @@
 package db
 
 import (
+	"context"
+	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/redis/go-redis/v9"
 	"log"
 	"sync"
 )
+
+type Postgres struct {
+	Db *pgxpool.Pool
+}
+
+var (
+	pgInstance *Postgres
+	pgOnce     sync.Once
+)
+
+func NewPG(ctx context.Context, connString string) (*Postgres, error) {
+	pgOnce.Do(func() {
+		db, err := pgxpool.New(ctx, connString)
+		if err != nil {
+			return
+		}
+		pgInstance = &Postgres{db}
+	})
+
+	return pgInstance, nil
+}
+
+func Close(pg *Postgres) {
+	pg.Db.Close()
+}
 
 type Redis struct {
 	Client *redis.Client
